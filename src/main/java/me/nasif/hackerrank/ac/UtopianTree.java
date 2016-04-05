c672e1d3162ce102e3aa4179522150ec8d58ff61
Nasif Ahmed <iamnasif@gmail.com>
2016-04-05 23:32:16 +0600

    HackerRank UtopianTree AC

---
+++ src/main/java/me/nasif/hackerrank/ac/UtopianTree.java
@@ -0,0 +1,35 @@
+package me.nasif.hackerrank.ac;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class UtopianTree {
+
+    private static Scanner scanner;
+
+    private static void initScanner() {
+        try {
+            scanner = new Scanner(new File(UtopianTree.class.getClassLoader().getResource("HackerRank/UtopianTree-in.txt").getFile()));
+        } catch (Exception ex) {
+            scanner = new Scanner(System.in);
+        }
+    }
+
+    public static void main(String[] args) {
+        initScanner();
+        int cases, n;
+        cases = scanner.nextInt();
+        while (cases-- != 0) {
+            int h = 1;
+            n = scanner.nextInt();
+            for (int i = 0; i < n; i++) {
+                if (i % 2 == 0) {
+                    h *= 2;
+                } else {
+                    h += 1;
+                }
+            }
+            System.out.println(h);
+        }
+    }
+}
