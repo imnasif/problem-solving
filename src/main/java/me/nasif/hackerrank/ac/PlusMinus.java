9a7395855e5541fb92c8c81d12d7ef07d8926c03
Nasif Ahmed <nasif.ahmed@infolytx.com>
2016-04-05 12:20:25 +0600

    HackerRack Plus Minus AC

---
+++ src/main/java/me/nasif/hackerrank/ac/PlusMinus.java
@@ -0,0 +1,39 @@
+package me.nasif.hackerrank.ac;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class PlusMinus {
+
+    private static Scanner scanner;
+
+    private static void initScanner() {
+        try {
+            scanner = new Scanner(new File(PlusMinus.class.getClassLoader().getResource("HackerRank/PlusMinus-in.txt").getFile()));
+        } catch (Exception ex) {
+            scanner = new Scanner(System.in);
+        }
+    }
+
+    public static void main(String[] args) {
+        initScanner();
+        int n = scanner.nextInt();
+        float plus = 0, minus = 0, zero = 0;
+        int i = n;
+        while (i-- != 0) {
+            float num = scanner.nextFloat();
+            if (num > 0) {
+                ++plus;
+            } else if (num < 0) {
+                ++minus;
+            } else {
+                ++zero;
+            }
+        }
+
+        System.out.println(plus / n);
+        System.out.println(minus / n);
+        System.out.println(zero / n);
+
+    }
+}
