0f1a8d2a732b875ce47c03bd5a09b85c13ac35e3
imnasif <iamnasif@gmail.com>
2016-04-10 16:30:45 +0600

    HR Algo LarrysArray AC

---
+++ src/main/java/me/nasif/hackerrank/algo/wa/LarrysArray.java
@@ -0,0 +1,38 @@
+package me.nasif.hackerrank.algo.wa;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class LarrysArray {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scan = new Scanner(new File(LarrysArray.class.getClassLoader().getResource("HackerRank/LarrysArray-in.txt").getFile()));
+//        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        while (t-- != 0) {
+            int n = scan.nextInt();
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = scan.nextInt();
+            }
+
+            int inversions = 0;
+            for (int i = 0; i < n - 1; i++) {
+                for (int j = i + 1; j < n; j++) {
+                    if (a[i] > a[j]) {
+                        ++inversions;
+                    }
+                }
+            }
+
+            if (inversions % 2 == 0) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+
+        }
+
+    }
+}
