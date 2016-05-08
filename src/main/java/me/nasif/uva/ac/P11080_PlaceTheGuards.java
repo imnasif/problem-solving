a28fb966fe0d323f3cceaa363adac71bc3c6ab12
imnasif <iamnasif@gmail.com>
2016-05-08 16:52:02 +0600

    UVA Place The Guards AC

--- src/main/java/me/nasif/uva/ac/P11080_PlaceTheGuards.java
+++ src/main/java/me/nasif/uva/ac/P11080_PlaceTheGuards.java
@@ -7,14 +7,11 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.Map;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class P924_SpreadingTheNews {
+public class P11080_PlaceTheGuards {
 
     static class Sc {
 
@@ -57,71 +54,81 @@ public class P924_SpreadingTheNews {
 
     private static Sc read;
     private static final PrintWriter write = new PrintWriter(new BufferedOutputStream(System.out), true);
-    private static int v, nq;
+    private static int n, v, e;
     private static boolean[][] g;
-    private static int[] d;
-    private static Queue<Integer> q;
+    private static boolean[] visited;
 
     public static void main(String[] args) throws FileNotFoundException, IOException {
-        read = new Sc("res/UVA/924.txt");
+        read = new Sc("res/UVA/11080.txt");
 //        read = new Sc();
-        v = read.nextInt();
-        g = new boolean[v][v];
+        n = read.nextInt();
+        while (n-- != 0) {
+            v = read.nextInt();
+            e = read.nextInt();
+            g = new boolean[v][v];
+            visited = new boolean[v];
+
+            while (e-- != 0) {
+                int v1 = read.nextInt();
+                int v2 = read.nextInt();
+                g[v1][v2] = true;
+                g[v2][v1] = true;
+            }
+
+            System.out.println(getResult());
+
+        }
+
+    }
+
+    private static int getResult() {
+        int res = 0;
         for (int i = 0; i < v; i++) {
-            int nf = read.nextInt();
-            while (nf-- != 0) {
-                int f = read.nextInt();
-                g[i][f] = true;
+            if (!visited[i]) {
+                int c = getMin(i);
+                if (c == -1) {
+                    res = -1;
+                    break;
+                }
+                res += c;
             }
         }
-        nq = read.nextInt();
-        while (nq-- != 0) {
-            int src = read.nextInt();
-            d = new int[v];
-            Arrays.fill(d, -1);
-            q = new LinkedList<>();
-            q.add(src);
-            d[src] = 0;
-            while (!q.isEmpty()) {
-                int cur = q.remove();
-                for (int i = 0; i < v; i++) {
-                    if (g[cur][i] && d[i] == -1) {
-                        q.add(i);
-                        d[i] = d[cur] + 1;
+        return res;
+    }
+
+    private static int getMin(int src) {
+
+        int[] color = new int[v];
+        Queue<Integer> q = new LinkedList<>();
+        q.add(src);
+        color[src] = 1;
+        while (!q.isEmpty()) {
+            int cur = q.remove();
+            visited[cur] = true;
+            for (int i = 0; i < v; i++) {
+                if (g[cur][i] && !visited[i]) {
+                    if (color[i] == color[cur]) { // not bipartite
+                        return -1;
                     }
+                    q.add(i);
+                    color[i] = -1 * color[cur];
                 }
             }
-            Arrays.sort(d);
-            if (d[d.length - 1] == 0) {
-                write.printf("%d\n", 0);
-                continue;
-            }
-
-            Map<Integer, Integer> m = new HashMap<>();
-            for (int i = 0; i < d.length; i++) {
-                if (d[i] > 0) {
-                    if (!m.containsKey(d[i])) {
-                        m.put(d[i], 1);
-                    } else {
-                        m.put(d[i], m.get(d[i]) + 1);
-                    }
-                }
-
-            }
-            int maxBoom = -1;
-            int day = 0;
-
-            for (Map.Entry<Integer, Integer> e : m.entrySet()) {
-                if (e.getValue() > maxBoom) {
-                    maxBoom = e.getValue();
-                    day = e.getKey();
-                }
-            }
-
-            write.printf("%d %d\n", maxBoom, day);
-
         }
 
+        int c1 = 0;
+        int c2 = 0;
+        for (int i = 0; i < color.length; i++) {
+            if (color[i] > 0) {
+                ++c1;
+            }
+            if (color[i] < 0) {
+                ++c2;
+            }
+
+        }
+        int min = Math.min(c1, c2);
+        return min == 0 ? 1 : min;
     }
 
 }
